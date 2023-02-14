da6e91b29723132ebfa249edf2df93a529c06b9b
Abdalrahman Alkewaifi <abood905000@gmail.com>
2023-02-13 18:05:18 -0600

    the new changes with the boarding page

--- lib/componants/home_screen.dart
+++ lib/componants/home_screen.dart
@@ -1,26 +1,102 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// import 'package:torch_controller/torch_controller.dart';
-import 'package:flutter_app_2/componants/camera_page.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'dart:io';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key, required this.camera1});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, this.title}) : super(key: key);
 
-  final CameraDescription camera1;
+  final String? title;
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  // final controller = TorchController();
-  bool flash = false;
+class _MyHomePageState extends State<MyHomePage> {
+  List<XFile>? _imageFileList;
+
+  void _setImageFileListFromFile(XFile? value) {
+    _imageFileList = value == null ? null : <XFile>[value];
+  }
+
+  dynamic _pickImageError;
+  bool isVideo = false;
+  String? _retrieveDataError;
+
+  final ImagePicker _picker = ImagePicker();
+  final TextEditingController maxWidthController = TextEditingController();
+  final TextEditingController maxHeightController = TextEditingController();
+  final TextEditingController qualityController = TextEditingController();
+
+  Future<void> _onImageButtonPressed(ImageSource source,
+      {BuildContext? context}) async {
+    final XFile? pickedFile = await _picker.pickImage(
+      source: source,
+    );
+    setState(() {
+      _setImageFileListFromFile(pickedFile);
+    });
+  }
+
+  Widget _previewImages() {
+    final Text? retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    if (_imageFileList != null) {
+      return Semantics(
+        label: 'image_picker_example_picked_images',
+        child: ListView.builder(
+          key: UniqueKey(),
+          itemBuilder: (BuildContext context, int index) {
+            // Why network for web?
+            // See https://pub.dev/packages/image_picker#getting-ready-for-the-web-platform
+            return Semantics(
+              label: 'image_picker_example_picked_image',
+              child: Image.file(File(_imageFileList![index].path)),
+            );
+          },
+          itemCount: _imageFileList!.length,
+        ),
+      );
+    } else if (_pickImageError != null) {
+      return Text(
+        'Pick image error: $_pickImageError',
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return const Text(
+        'You have not yet picked an image.',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  Widget _handlePreview() {
+    return _previewImages();
+  }
+
+  Future<void> retrieveLostData() async {
+    final LostDataResponse response = await _picker.retrieveLostData();
+    if (response.isEmpty) {
+      return;
+    }
+    if (response.file != null) {
+      setState(() {
+        if (response.files == null) {
+          _setImageFileListFromFile(response.file);
+        } else {
+          _imageFileList = response.files;
+        }
+      });
+    } else {
+      _retrieveDataError = response.exception!.code;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // getCamera();
     return Scaffold(
       appBar: AppBar(
         leading: Semantics(
@@ -78,38 +154,8 @@ class _HomeScreenState extends State<HomeScreen> {
                           shrinkWrap: true,
                           children: <Widget>[
                             const SizedBox(height: 20),
-                            const Center(child: Text('Customize')),
+                            const Center(child: Text('Settings')),
                             const SizedBox(height: 20),
-                            // TextButton.icon(
-                            //   onPressed: () {},
-                            //   label: const Text(
-                            //     'Save to Gallery',
-                            //     style: TextStyle(
-                            //         fontSize: 15, fontWeight: FontWeight.bold),
-                            //   ),
-                            //   icon: const Icon(
-                            //     Icons.save_alt,
-                            //     size: 24.0,
-                            //   ),
-                            //   style: TextButton.styleFrom(
-                            //       foregroundColor: Colors.black,
-                            //       alignment: Alignment.centerLeft),
-                            // ),
-                            TextButton.icon(
-                              onPressed: () {},
-                              label: const Text(
-                                'Change Voice',
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              icon: const Icon(
-                                Icons.man_outlined,
-                                size: 24.0,
-                              ),
-                              style: TextButton.styleFrom(
-                                  foregroundColor: Colors.black,
-                                  alignment: Alignment.centerLeft),
-                            ),
                             TextButton.icon(
                               onPressed: () {},
                               label: const Text(
@@ -138,9 +184,63 @@ class _HomeScreenState extends State<HomeScreen> {
               ))
         ],
       ),
-      body: TakePictureScreen(
-        camera: widget.camera1, // pass camera to widget
+      body: Center(
+        child: !kIsWeb && defaultTargetPlatform == TargetPlatform.android
+            ? FutureBuilder<void>(
+                future: retrieveLostData(),
+                builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return const Text(
+                        'You have not yet picked an image.',
+                        textAlign: TextAlign.center,
+                      );
+                    case ConnectionState.done:
+                      return _handlePreview();
+                    default:
+                      if (snapshot.hasError) {
+                        return Text(
+                          'Pick image/video error: ${snapshot.error}}',
+                          textAlign: TextAlign.center,
+                        );
+                      } else {
+                        return const Text(
+                          'You have not yet picked an image.',
+                          textAlign: TextAlign.center,
+                        );
+                      }
+                  }
+                },
+              )
+            : _handlePreview(),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                isVideo = false;
+                _onImageButtonPressed(ImageSource.camera, context: context);
+              },
+              heroTag: 'image2',
+              tooltip: 'Take a Photo',
+              child: const Icon(Icons.camera_alt),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Text? _getRetrieveErrorWidget() {
+    if (_retrieveDataError != null) {
+      final Text result = Text(_retrieveDataError!);
+      _retrieveDataError = null;
+      return result;
+    }
+    return null;
   }
 }
